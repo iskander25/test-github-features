3cb7a4413a2b11a3aae621a31eb641451358c4f6
Youssef harrabi <youssef.harrabi@esprit.tn>
2026-02-15 20:27:17 +0100

    fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,7 +15,7 @@ describe('AppComponent', () => {
   it(`should have as title 'test-github'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('test');
+    expect(app.title).toEqual('test-github');
   });
 
   it('should render title', () => {
