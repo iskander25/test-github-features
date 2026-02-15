ff124e3e86b8336309d363465874e0ce4f702b01
Youssef harrabi <youssef.harrabi@esprit.tn>
2026-02-15 21:04:11 +0100

    fix: update app title in tests to 'testt-githubb'

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'test-github'`, () => {
+  it(`should have as title 'testt-githubb'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('test-github');
+    expect(app.title).toEqual('testt-githubb');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('test-github app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('testt-githubb app is running!');
   });
 });
